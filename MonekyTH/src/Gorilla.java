f0250e921b5a6995b09f964fc5014822febfe977
Hadi Abu Hamed <hadi.abuhamed@galilsoftware.com>
2015-10-21 15:26:23 +0300

    with getters and setters

--- MonekyTH/src/Gorilla.java
+++ MonekyTH/src/Gorilla.java
@@ -1,4 +1,34 @@
 
 public class Gorilla {
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	private int num;
+	private String name;
+	private int id;
+	
+	public Gorilla()
+	{}
+	public Gorilla(int num,String name, int id)
+	{
+		this.num=num;
+		this.name=name;
+		this.id=id;
+	}
 
 }
