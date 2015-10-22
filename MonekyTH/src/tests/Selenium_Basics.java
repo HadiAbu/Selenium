c18debd1fc2bd207feac1c1c82bc4ee4a5b890f6
Hadi Abu Hamed <hadi.abuhamed@galilsoftware.com>
2015-10-22 17:15:15 +0300

    cisco search

--- MonekyTH/src/tests/Selenium_Basics.java
+++ MonekyTH/src/tests/Selenium_Basics.java
@@ -1,12 +1,34 @@
 package tests;
 
-import java.io.File;
-
-import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Test;
 
 public class Selenium_Basics {
 
-	FirefoxBinary driverBin = new FirefoxBinary(new File("C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe"));
-	FirefoxDriver driver = new FirefoxDriver(driverBin,null);
+//	FirefoxBinary driverBin = new FirefoxBinary(new File("C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe"));
+//	FirefoxDriver driver = new FirefoxDriver(driverBin,null);
+	
+	FirefoxDriver driver = new FirefoxDriver();
+	
+	//chrome
+	//System.setProperty("webdriver.chrome.driver","C:");
+	ChromeDriver cd = new ChromeDriver();
+	
+	//ie
+	InternetExplorerDriver ieDriver = new InternetExplorerDriver();
+	@Test
+	public void test(){
+		driver.get("http://www.google.com");
+		driver.manage().window().maximize();
+		WebElement element = driver.findElement(By.id("lst-ib"));
+		element.clear();
+		element.sendKeys("Cisco");
+		WebElement element2 = driver.findElement(By.name("btnK"));
+		element2.click();
+		
+	}
 }
