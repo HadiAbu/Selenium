d8e76b9642ac2c31c61f6bff67a0db4917d53998
Hadi Abu Hamed <hadi.abuhamed@galilsoftware.com>
2015-10-22 11:59:04 +0300

    ..

---
+++ MonekyTH/src/MainPack/FilesShit.java
@@ -0,0 +1,67 @@
+package MainPack;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FilesShit
+{
+	//for character stream reading
+	private FileReader fileReader;
+	private BufferedReader buffReader;
+	//for character stream writing
+	private FileWriter fileWriter;
+	private BufferedWriter buffWriter;
+
+	//for binary stream reading
+	private FileInputStream fis;
+	//for binary stream writing
+	private FileOutputStream fos;
+	
+	public FilesShit() throws IOException
+	{
+//		initReaders();
+		initWriters();
+	}
+	public void initReaders() throws IOException{
+		fileReader = new FileReader("C:\\Users\\Galil1\\Documents\\Hadi\\myfile.txt");
+		fis = new FileInputStream("C:\\Users\\Galil1\\Documents\\Hadi\\myBinfile.dat");
+	}
+	public void initWriters() throws IOException{
+		fileWriter = new FileWriter("C:\\Users\\Galil1\\Documents\\Hadi\\myfile.txt");
+		fos = new FileOutputStream("C:\\Users\\Galil1\\Documents\\Hadi\\myBinfile.dat");
+	}
+	public void WriteStream()
+	{
+		
+	}
+	public void WriteCharacters(String s) throws IOException
+	{
+		fileWriter.write(s);
+	}
+	public void CloseFiles() throws IOException
+	{
+//		fileReader.close();
+//		fis.close();
+		fileWriter.close();
+		fos.close();
+	}
+	protected void finialize() throws Throwable
+	{
+		super.finalize();
+		fileReader.close();
+		fis.close();
+		fileWriter.close();
+		fos.close();
+	}
+	public static void main(String []args) throws IOException{
+		System.out.println("Hello World!");
+		FilesShit fs = new FilesShit();
+		fs.WriteCharacters("Hello World!");
+		fs.CloseFiles();
+	}
+}
--- MonekyTH/src/MainPack/Monster.java
+++ MonekyTH/src/MainPack/Monster.java
@@ -1,16 +1,36 @@
 package MainPack;
-public class Monster {
 
-	public static int check2Plus2() {
-		return 0;
+import java.util.Scanner;
+import java.io.*;
+
+
+public class Monster {
+	enum Action{
+		Play,
+		Sing,
+		Fight,
+		Introduce
 	}
 
-	// public static int check2Plus2() {
-	// }
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
+		Exception ex = new Exception("Something is really fucked up here!");
+		Scanner s = new Scanner(System.in);
 		System.out.println("Hello World!");
+		System.out.println("Please enter your age: ");
+		String age = s.next();
+		System.out.println("The age you entered is: "+age);
+//		int yourAge = Integer.parseInt(age);
+		System.out.println("Plus my age: "+(Integer.parseInt(age)+23));
+		try{
+		Thread.sleep(10);
+		}
+		catch(Exception e)
+		{}
+		finally{
+			
+		}
+		
 	}
 
 }
